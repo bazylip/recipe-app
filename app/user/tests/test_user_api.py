9be227a86539f4805d04c72627e9880974ca0c96
Bazyli Polednia <bazyli.polednia@gmail.com>
2023-07-03 22:19:00 +0200

    Add API for user creation

---
+++ app/user/tests/test_user_api.py
@@ -0,0 +1,52 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+
+CREATE_USER_URL = reverse("user:create")
+
+
+def create_user(**kwargs):
+    return get_user_model().objects.create_user(**kwargs)
+
+
+class PublicUserAPITests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test__create_user__success(self):
+        payload = {
+            "email": "test@example.com",
+            "password": "testpass123",
+            "name": "Test Name",
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(email=payload["email"])
+        self.assertTrue(user.check_password(payload["password"]))
+        self.assertNotIn("password", res.data)
+
+    def test__create_user_with_email_already_exists__error(self):
+        payload = {
+            "email": "test@example.com",
+            "password": "testpass123",
+            "name": "Test Name",
+        }
+        create_user(**payload)
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test__create_user_with_password_too_short__error_and_does_not_create_user(self):
+        payload = {
+            "email": "test@example.com",
+            "password": "pw",
+            "name": "Test Name",
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(email=payload["email"]).exists()
+        self.assertFalse(user_exists)
